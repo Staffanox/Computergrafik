033c69f8a6ae4b5285dac1bda3773dcef61d8437
Steven <Steven@STAFFANO>
2020-03-10 19:11:23 +0100

    fixes lighting ans visualization

--- Computergrafik/fragment.fs
+++ Computergrafik/fragment.fs
@@ -1,7 +1,5 @@
 #version 330 core
 out vec4 FragColor;
-out vec4 FragColor2;
-
 
 struct Material {
     sampler2D diffuse;
@@ -19,6 +17,7 @@ struct DirLight {
 
 
 
+
 in vec3 FragPos;
 in vec3 Normal;
 in vec2 TexCoords;
@@ -27,6 +26,7 @@ uniform vec3 viewPos;
 uniform DirLight dirLight;
 uniform Material material;
 
+// function prototypes
 vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir);
 
 void main()
@@ -35,17 +35,9 @@ void main()
     vec3 norm = normalize(Normal);
     vec3 viewDir = normalize(viewPos - FragPos);
     
-    // == =====================================================
-    // Our lighting is set up in 3 phases: directional, point lights and an optional flashlight
-    // For each phase, a calculate function is defined that calculates the corresponding color
-    // per lamp. In the main() function we take all the calculated colors and sum them up for
-    // this fragment's final color.
-    // == =====================================================
-    // phase 1: directional lighting
     vec3 result = CalcDirLight(dirLight, norm, viewDir);
-
+    
     FragColor = vec4(result, 1.0);
-
 }
 
 // calculates the color when using a directional light.
@@ -63,4 +55,3 @@ vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir)
     vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
     return (ambient + diffuse + specular);
 }
-
